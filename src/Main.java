06f35e3da64f526b812667796c6937fbd1a590e8
Westender <javaxwestender@gmail.com>
2023-08-02 21:15:54 +0300

    Add: task1

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,38 @@
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
-    public static void main(String[] args) {
 
+    static int counter = 1;
+
+    public static void main(String[] args) {
+        task1();
     }
 
     public static int[] generateRandomArray(int lenght) {
         int[] array = new int[lenght];
         for (int i = 0; i < array.length; i++) {
             // подсмотрел в доп заданиях про TLRnd, что лучше использовать его
-            array[i] = ThreadLocalRandom.current().nextInt(100_000, 200_000+1);
+            array[i] = ThreadLocalRandom.current().nextInt(100_000, 200_000 + 1);
 
         }
         return array;
     }
 
+    public static int sumOfArray(int[] array) {
+        int result = 0;
+
+        for (int i : array) {
+            result += i;
+        }
+
+        return result;
+    }
+
+    static void task1() {
+        System.out.printf("\nЗадание %d:", counter++);
+
+        int[] book = generateRandomArray(30);
+
+        System.out.printf("\nСумма трат за месяц составила %d рублей\n", sumOfArray(book));
+    }
 }
