baad09fffb929802e9507207b3618696e75d6a9a
Westender <javaxwestender@gmail.com>
2023-08-02 21:07:22 +0300

    Add: method generateRandomArray

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static int[] generateRandomArray(int lenght) {
+        int[] array = new int[lenght];
+        for (int i = 0; i < array.length; i++) {
+            // подсмотрел в доп заданиях про TLRnd, что лучше использовать его
+            array[i] = ThreadLocalRandom.current().nextInt(100_000, 200_000+1);
+
+        }
+        return array;
+    }
+
 }
